4ec7ded7f6ad2bab7f9d29fc50206388cdf04d94
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-27 13:06:22 -0700

    imported dashboard into app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import SignInSide from './components/auth/SignInSide';
 import SignUp from './components/auth/SignUp';
 import Checkout from './components/checkout/Checkout';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <div>
       {/* <SignInSide /> */}
       {/* <SignUp /> */}
-      <Checkout />
+      {/* <Checkout /> */}
+      <Dashboard />
     </div>
   );
 }
