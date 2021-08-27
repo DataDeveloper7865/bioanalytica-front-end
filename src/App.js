f826747922087df62e04f52f6a0cdd7ba158845e
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-27 12:11:30 -0700

    added sign in component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SignInSide from './components/SignInSide';
 
 function App() {
   return (
     <div>
-      hello
+      <SignInSide />
     </div>
   );
 }
