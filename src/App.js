6feb47c264dfcc5bf560b4c06a90ac9679d927f0
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-27 12:43:31 -0700

    added checkout to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import SignInSide from './components/auth/SignInSide';
 import SignUp from './components/auth/SignUp';
+import Checkout from './components/checkout/Checkout';
 
 function App() {
   return (
     <div>
       {/* <SignInSide /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <Checkout />
     </div>
   );
 }
