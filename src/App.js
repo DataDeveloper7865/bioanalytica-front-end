4d895c8fd63e4800f8f579e658744f0fee60607c
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-30 15:20:34 -0700

    moved routes into router component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import SignInSide from './components/auth/SignInSide';
-import SignUp from './components/auth/SignUp';
-import Checkout from './components/checkout/Checkout';
-import Dashboard from './components/dashboard/Dashboard';
-import Pricing from './components/pricing/Pricing';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import Routes from './components/routing/Routes';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/signin">
-          <SignInSide /> 
-        </Route>
-        <Route path="/checkout">
-          <Checkout />  
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/pricing">
-          <Pricing />
-        </Route>
-      </Switch>
-    </Router>
+    <Routes/>
   );
 }
 
