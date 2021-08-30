adf38558f2e03223a52e37c04231e5cbab963364
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-30 15:25:48 -0700

    added routing

--- src/components/routing/Routes.js
+++ src/components/routing/Routes.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import SignInSide from '../auth/SignInSide';
+import SignUp from '../auth/SignUp';
+import Checkout from '../checkout/Checkout';
+import Dashboard from '../dashboard/Dashboard';
+import Pricing from '../pricing/Pricing';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function Routes () {
 
     return (
-
-        <div>
-            howdy
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/signup">
+                    <SignUp />
+                </Route>
+                <Route path="/signin">
+                    <SignInSide /> 
+                </Route>
+                <Route path="/checkout">
+                    <Checkout />  
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+                <Route path="/pricing">
+                    <Pricing />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
