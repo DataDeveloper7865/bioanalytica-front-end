ef5223598679dc6d949c9fd3810cb2343bfbed9c
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-31 13:41:14 -0700

    added context provider and token

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import SignInSide from './components/auth/SignInSide';
 import Routes from './components/routing/Routes';
+import React, { useState, useEffect }  from 'react';
 
 function App() {
+  const [token, setToken] = useState();
+
+  const MyContext = React.createContext(token);
+
+  useEffect(() => {
+    console.log("THE TOKEN IS: ", token)
+  }, [token])
+
+  if(!token) {
+    return <SignInSide setToken={setToken}/>
+  }
+
+
   return (
-    <Routes/>
+    <MyContext.Provider value={token}>
+      <Routes/>
+    </MyContext.Provider>
   );
 }
 
